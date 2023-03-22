29e7a45cde1821fd401e4599d0d24c858d5624d0
uzrafi <uzairrafi159@gmail.com>
2023-03-21 22:20:05 -0400

    added register page

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,24 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../Styles/Register.scss";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  useState(() => {});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div className="Register">
       <Navbar></Navbar>
       <div className="Container">
-
         <label className="LoginLabel">Login</label>
 
-        <input className="Input" placeholder="Email"></input>
+        <input 
+        className="Input" 
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}>
+        </input>
 
-        <input className = "Input" placeholder="Password"></input>
+        <input 
+        className="Input" 
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}>
+        </input>
 
-        <a href="" className="ForgotPassLabel">Forgot your password?</a>
-        <button className="Button" onClick="">Sign in</button>
-        <a href="" className="CreateAccLabel">Create account</a>
+        <a href="" className="ForgotPassLabel">
+          Forgot your password?
+        </a>
+        <button className="Button" onClick="">
+          Log in
+        </button>
+        <label className="CreateAccLabel">Don't have an account?</label>
+
+        <div className="SignUpLabel">
+          <a href="">
+            <Link to="/Register">Sign up</Link>
+          </a>
+        </div>
+
       </div>
     </div>
   );
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../Styles/Register.scss";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
-const Register = () => {
+const Register = () => {  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [valid, setValid] = useState('');
+
   return (
-    <div>
-        Register
-    </div>
-  )
-}
+    <div className="Register">
+      <Navbar></Navbar>
+      <div className="Container">
+        <label className="LoginLabel">Sign Up</label>
 
-export default Register
+        <input 
+        className="Input" 
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}>
+        </input>
+
+        <input 
+        className="Input" 
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}>
+        </input>
+
+        <input 
+        className="Input" 
+        placeholder="Re-enter Password"
+        onChange={(e) => setValid(e.target.value)}>
+        </input>
+
+        <button className="Button" onClick="">
+          Sign up
+        </button>
+        <label className="CreateAccLabel">Already have an account?</label>
+
+
+        <div className="SignUpLabel">
+          <a href="">
+            <Link to="/Login">Login</Link>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
+
