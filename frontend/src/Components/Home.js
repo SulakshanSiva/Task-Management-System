ad550ac23bb71551c036666afb4482bb69ebb68c
Ryan Nguyen <rnguye06@uoguelph.ca>
2023-05-05 23:01:50 -0400

    final merge slipup

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -30,82 +30,87 @@ const Home = () => {
   const initialColumns = {
     todo: {
       id: 'todo',
-      cards: ['item 1', 'item 2', 'item 3']
+      cards: ['item 1', 'item 2', 'item 3'],
     },
     doing: {
       id: 'doing',
-      cards: []
+      cards: [],
     },
     done: {
       id: 'done',
-      cards: []
-    }
-  }
-  const [columns, setColumns] = useState(initialColumns)
+      cards: [],
+    },
+  };
+
+  const [columns, setColumns] = useState(initialColumns);
 
   const onDragEnd = ({ source, destination }) => {
     // Make sure we have a valid destination
     if (destination === undefined || destination === null) return null;
   
     // Make sure we're actually moving the item
-    if (source.droppableId === destination.droppableId &&
-      destination.index === source.index) return null;
-
+    if (
+      source.droppableId === destination.droppableId &&
+      destination.index === source.index
+    ) return null;
+  
     // Set start and end variables
-    const start = columns[source.droppableId]
-    const end = columns[destination.droppableId]
-    
+    const start = columns[source.droppableId];
+    const end = columns[destination.droppableId];
+  
+    console.log(`Dragged Card ID: ${start.cards[source.index]}`);
+    console.log(`Source Column: ${start.id}`);
+    console.log(`Destination Column: ${end.id}`);
+  
+  
     // If start is the same as end, we're in the same column
     if (start === end) {
       // Move the item within the list
-      // Start by making a new list without the dragged item
-      const newCards = cards.filter((_, idx) => idx !== source.index);
-    
-      // Then insert the item at the right location
-      newCards.splice(destination.index, 0, cards[source.index]);
-
-      
-      // Then create a new copy of the column object
+      const newCards = Array.from(start.cards);
+      const [removedCard] = newCards.splice(source.index, 1);
+      newCards.splice(destination.index, 0, removedCard);
+  
       const newCol = {
-        id: start.id,
-        cards: newCards
-      }
-    
-      // Update the list
-      setCards(state => ({ ...state, [newCol.id]: newCol }));
-      return null;
+        ...start,
+        cards: newCards,
+      };
+  
+      setColumns(state => ({
+        ...state,
+        [newCol.id]: newCol,
+      }));
     } else {
-      const newStartCards = start.cards.filter((_, idx) => idx !== source.index);
-      
-      // Create a new start column
+      // Moving between columns
+      const startCards = Array.from(start.cards);
+      const [removedCard] = startCards.splice(source.index, 1);
+  
+      const endCards = Array.from(end.cards);
+      endCards.splice(destination.index, 0, removedCard);
+  
       const newStartCol = {
-        id: start.id,
-        cards: newStartCards
-      }
-
-      // Make a new end list array
-      const newEndCards = end.cards
-
-      // Insert the item into the end list
-      newEndCards.splice(destination.index, 0, start.cards[source.index])
-
-      // Create a new end column
+        ...start,
+        cards: startCards,
+      };
+  
       const newEndCol = {
-        id: end.id,
-        cards: newEndCards
-      }
-      // Update the state
-      setColumns(state => ({...state, [newStartCol.id]: newStartCol, [newEndCol.id]: newEndCol}))
-      return null;
+        ...end,
+        cards: endCards,
+      };
+  
+      setColumns(state => ({
+        ...state,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol,
+      }));
     }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="home">
-      <Navbar/>
+      {/* <Navbar/> */}
         {Object.values(columns).map((column) => (
-          <Column key={column.id} column={column}/>
+          <Column key={column.id} column={column} />
         ))}
       </div>
     </DragDropContext>
